541ebd892b6ef392952a78dbbf9e8b1a3ea19e25
ripterdust <bryantello2010@hotmail.com>
2020-09-20 10:41:18 -0600

    Agregada la clase de estaciones y está hecha la petición al servidor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Clases
-
+// FIXME - Modularizar cada clase para no tener problema
 // ANCHOR - clase de alumno
 class Alumno{
     constructor(nombre, carnet){
@@ -17,11 +17,31 @@ class Alumno{
 
 // ANCHOR - clase de las gráficas
 
+class Estacion{
+    constructor(apiLink){
+        this.__link = apiLink;
 
+    }
 
+    get datosEstacion(){
+        let datos;
+        fetch(link)
+            .then(res => res.json())
+            .catch(err => console.log(err))
+            .then(data => datos = data);
 
+        return data;
+    }
+}
+
+// Instanciando alumnos
 const bryan = new Alumno('Bryan Arévalo', 201945176);
 const luis = new Alumno('Luis Pinzón', 5213581);
 
 console.log(bryan.datos.nombre);
 
+// Instanciando estaciones
+let linkBryan = 'https://api.thingspeak.com/channels/1147595/feeds.json?results=2';
+
+
+    
