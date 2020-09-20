b79c418ef0466176bbbce67252c2972715176f7f
ripterdust <bryantello2010@hotmail.com>
2020-09-20 10:47:29 -0600

    Arreglados los bugs al implementar la clase 'Estacion', sólo hace falta corregir el bug del undefined

--- js/main.js
+++ js/main.js
@@ -21,13 +21,15 @@ class Estacion{
     }
 
     get datosEstacion(){
-        let datos;
-        fetch(link)
+        var datos;
+        fetch(this.__link)
             .then(res => res.json())
             .catch(err => console.log(err))
-            .then(data => datos = data);
-
-        return data;
+            .then(data => { 
+                datos = data;
+            });
+        console.log(datos)
+        return datos;
     }
 }
 
@@ -41,6 +43,6 @@ console.log(bryan.datos.nombre);
 
 // Instanciando estaciones
 let linkBryan = 'https://api.thingspeak.com/channels/1147595/feeds.json?results=2';
-
-
+const estacionBryan = new Estacion(linkBryan);
+console.log(estacionBryan.datosEstacion);
     
