19ef2f2e9be405a8184a89abfaf54cf842ad1fb9
ripterdust <bryantello2010@hotmail.com>
2020-09-20 10:29:11 -0600

    agregada la clase de alumno en el archivo principal, cuando se termine de programar el proyecto voy a modularizarlo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-import Alumno from './class/alumno';
+// Clases
+
+// ANCHOR - clase de alumno
+class Alumno{
+    constructor(nombre, carnet){
+        this.nombre = nombre;
+        this.carnet = carnet;
+    }
+
+    get datos(){
+        return({
+            nombre: this.nombre,
+            carnet: this.carnet
+        });
+    }
+}
+
+// ANCHOR - clase de las gráficas
+
+
+
 
 const bryan = new Alumno('Bryan Arévalo', 201945176);
 const luis = new Alumno('Luis Pinzón', 5213581);
 
-console.log(bryan.datos);
+console.log(bryan.datos.nombre);
 
