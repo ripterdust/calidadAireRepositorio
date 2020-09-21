1aad9c8d59ab0be8eda5ca935326f1be9e89b2ec
ripterdust <bryantello2010@hotmail.com>
2020-09-20 18:53:11 -0600

    Agregado font awesome, y tratando de implementar el botón para ir hasta arriba

--- js/main.js
+++ js/main.js
@@ -70,8 +70,8 @@ new Morris.Line({
   // the chart.
   data: [
     { year: '2008', value: 20, value2: 30 },
-    { year: '2009', value: 10, value2: 40 },
-    { year: '2010', value: 5, value2: 50 },
+    { year: '2009', value: 10, value2: 3 },
+    { year: '2010', value: 5, value2: 5 },
     { year: '2011', value: 5, value2: 10 },
     { year: '2012', value: 20, value2: 20 }
   ],
