36bc0db417d5f072afab2dac1e894154a8542ff3
ripterdust <bryantello2010@hotmail.com>
2020-09-20 18:36:47 -0600

    Agregado comment achors y identado como se debe

--- js/main.js
+++ js/main.js
@@ -1,50 +1,110 @@
-// Gráficas Bryan
-new Morris.Line({
-    // ID of the element in which to draw the chart.
-    element: 'temperaturaBryan',
-    // Chart data records -- each entry in this array corresponds to a point on
-    // the chart.
-    data: [
-      { year: '2008', value: 20, value2: 30 },
-      { year: '2009', value: 10, value2: 40 },
-      { year: '2010', value: 5, value2: 50 },
-      { year: '2011', value: 5, value2: 10 },
-      { year: '2012', value: 20, value2: 20 }
-    ],
-    // The name of the data record attribute that contains x-values.
-    xkey: 'year',
-    // A list of names of data record attributes that contain y-values.
-    ykeys: ['value', 'value2'],
-    // Labels for the ykeys -- will be displayed when you hover over the
-    // chart.
-    labels: ['temperatura', 'humedad'],
-    resize: true,
-    lineColors: ['#3498db', '#2CB4AC'],
-    lineWidth: 2
-  });
+// NOTE - Acá van las gráficas
 
 
-//   Gráficas Luis
+// SECTION - Gráficas Bryan
+
+// ANCHOR - Gráfica de temperatura y humedad
 new Morris.Line({
-    // ID of the element in which to draw the chart.
-    element: 'temperaturaLuis',
-    // Chart data records -- each entry in this array corresponds to a point on
-    // the chart.
-    data: [
-      { year: '2008', value: 20, value2: 30 },
-      { year: '2009', value: 10, value2: 40 },
-      { year: '2010', value: 5, value2: 50 },
-      { year: '2011', value: 5, value2: 10 },
-      { year: '2012', value: 20, value2: 20 }
-    ],
-    // The name of the data record attribute that contains x-values.
-    xkey: 'year',
-    // A list of names of data record attributes that contain y-values.
-    ykeys: ['value', 'value2'],
-    // Labels for the ykeys -- will be displayed when you hover over the
-    // chart.
-    labels: ['temperatura', 'humedad'],
-    resize: true,
-    lineColors: ['#3498db', '#2CB4AC'],
-    lineWidth: 2
-  });
+  // ID of the element in which to draw the chart.
+  element: 'temperaturaBryan',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2008', value: 20, value2: 30 },
+    { year: '2009', value: 10, value2: 40 },
+    { year: '2010', value: 5, value2: 50 },
+    { year: '2011', value: 5, value2: 10 },
+    { year: '2012', value: 20, value2: 20 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value', 'value2'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['temperatura', 'humedad'],
+  resize: true,
+  lineColors: ['#3498db', '#2CB4AC'],
+  lineWidth: 2
+});
+  // ANCHOR - Gráfica de partículas
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'particulasBryan',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2008', value: 20, value2: 30 },
+    { year: '2009', value: 10, value2: 40 },
+    { year: '2010', value: 5, value2: 50 },
+    { year: '2011', value: 5, value2: 10 },
+    { year: '2012', value: 20, value2: 20 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value', 'value2'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['temperatura', 'humedad'],
+  resize: true,
+  lineColors: ['#3498db', '#2CB4AC'],
+  lineWidth: 2
+});
+
+
+// SECTION - Gráficas Luis
+
+// ANCHOR - Gráfica de temperatura y humedad
+
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'temperaturaLuis',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2008', value: 20, value2: 30 },
+    { year: '2009', value: 10, value2: 40 },
+    { year: '2010', value: 5, value2: 50 },
+    { year: '2011', value: 5, value2: 10 },
+    { year: '2012', value: 20, value2: 20 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value', 'value2'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['temperatura', 'humedad'],
+  resize: true,
+  lineColors: ['#3498db', '#2CB4AC'],
+  lineWidth: 2
+});
+
+  // ANCHOR - Gráfica de partículas
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'particulasBryan',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2008', value: 20, value2: 30 },
+    { year: '2009', value: 10, value2: 40 },
+    { year: '2010', value: 5, value2: 50 },
+    { year: '2011', value: 5, value2: 10 },
+    { year: '2012', value: 20, value2: 20 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value', 'value2'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['temperatura', 'humedad'],
+  resize: true,
+  lineColors: ['#3498db', '#2CB4AC'],
+  lineWidth: 2
+});
