f8d426b24bd1a03cf09991b58c64997f7df9946b
ripterdust <bryantello2010@hotmail.com>
2020-09-20 18:46:18 -0600

    Agregadas las gráficas y puesto el hidehover para que el recuedro no se mostrara de manera preprpétua, sólo hace falta hacer que se actualice dinámicamente,

--- js/main.js
+++ js/main.js
@@ -25,7 +25,9 @@ new Morris.Line({
   labels: ['temperatura', 'humedad'],
   resize: true,
   lineColors: ['#3498db', '#2CB4AC'],
-  lineWidth: 2
+  lineWidth: 2,
+  hideHover: true
+
 });
   // ANCHOR - Gráfica de partículas
 
@@ -35,22 +37,24 @@ new Morris.Line({
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-    { year: '2008', value: 20, value2: 30 },
-    { year: '2009', value: 10, value2: 40 },
-    { year: '2010', value: 5, value2: 50 },
-    { year: '2011', value: 5, value2: 10 },
-    { year: '2012', value: 20, value2: 20 }
+    { year: '2008', value: 3000},
+    { year: '2009', value: 4500},
+    { year: '2010', value: 3500},
+    { year: '2011', value: 3800},
+    { year: '2012', value: 2500}
   ],
   // The name of the data record attribute that contains x-values.
   xkey: 'year',
   // A list of names of data record attributes that contain y-values.
-  ykeys: ['value', 'value2'],
+  ykeys: ['value'],
   // Labels for the ykeys -- will be displayed when you hover over the
   // chart.
-  labels: ['temperatura', 'humedad'],
+  labels: ['Partículas por millón: '],
   resize: true,
   lineColors: ['#3498db', '#2CB4AC'],
-  lineWidth: 2
+  lineWidth: 2,
+  hideHover: true
+
 });
 
 
@@ -80,7 +84,9 @@ new Morris.Line({
   labels: ['temperatura', 'humedad'],
   resize: true,
   lineColors: ['#3498db', '#2CB4AC'],
-  lineWidth: 2
+  lineWidth: 2,
+  hideHover: true
+
 });
 
   // ANCHOR - Gráfica de partículas
@@ -91,11 +97,11 @@ new Morris.Line({
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-    { year: '2008', value: 20, value2: 30 },
-    { year: '2009', value: 10, value2: 40 },
-    { year: '2010', value: 5, value2: 50 },
-    { year: '2011', value: 5, value2: 10 },
-    { year: '2012', value: 20, value2: 20 }
+    { year: '2008', value: 3900},
+    { year: '2009', value: 3100},
+    { year: '2010', value: 2500},
+    { year: '2011', value: 400},
+    { year: '2012', value: 1500}
   ],
   // The name of the data record attribute that contains x-values.
   xkey: 'year',
@@ -103,8 +109,9 @@ new Morris.Line({
   ykeys: ['value', 'value2'],
   // Labels for the ykeys -- will be displayed when you hover over the
   // chart.
-  labels: ['temperatura', 'humedad'],
+  labels: ['Partículas por millón: '],
   resize: true,
   lineColors: ['#3498db', '#2CB4AC'],
-  lineWidth: 2
+  lineWidth: 2,
+  hideHover: true
 });
